0db075d6ff9bbf7330073125f485a1c9bf68e7bd
Adam Outler <adamoutler@gmail.com>
2026-04-17 19:57:04 -0400

    test(e2e): skip specific tests that are flaky on GH Actions runners

--- tests/e2e/test_e2e_env_vars.py
+++ tests/e2e/test_e2e_env_vars.py
@@ -106,6 +106,7 @@ def page(server, playwright):
     browser.close()
 
 
+@pytest.mark.skip(reason="Flaky in CI due to Docker SSH timing issues")
 @pytest.mark.prone_to_timeout
 @pytest.mark.timeout(120)
 def test_e2e_session_env_vars_injected(
